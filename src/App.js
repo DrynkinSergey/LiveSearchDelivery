52de4acf9ffebd0d86994abc24e6de5a6ba0c98a
Sergey <sergsmile29@gmail.com>
2022-02-23 16:03:11 +0200

    added bd

--- src/App.js
+++ src/App.js
@@ -2,26 +2,12 @@ import './style.scss';
 
 import {useEffect, useState} from "react";
 import AddressItem from "./Component/AddressItem";
+import {data} from "./addres";
 
 function App() {
     const [value,setValue] = useState('');
 
-    const data = [{
-       address:'Ленина',
-        price:34
-    },
-        {
-            address:'Мира',
-            price:31
-        },
-        {
-            address:'Ленина',
-            price:32
-        },
-        {
-            address:'1го мая',
-            price:3
-        }]
+
     const [address, setState] = useState([]);
     const getState = () => {
         setState(data);
@@ -31,12 +17,14 @@ function App() {
         getState();
     }, []);
      const filteredAddress = address.filter(el=>{
-         return el.address.toLowerCase().includes(value.toLowerCase())
-     })
+         return el.address.toLowerCase().includes(value.toLowerCase()) || el.location.toLowerCase().includes(value.toLowerCase());
+     }
+)
 
     return (
         <div className="App">
             <h2>Начните набирать адресс:</h2>
+            <div>Найдено адрессов: {filteredAddress.length}</div>
             <input  type="text" onChange={(e)=> {
                 let className = 'drop';
                 e.currentTarget.classList.add(className)
--- src/addres.js
+++ src/addres.js
@@ -1,3 +1,441 @@
-console.log(1)
+const basePrice = 40;
+const baseFive = 45;
 
 
+export const data = [
+    // Черноморск
+    {
+        address: 'Утренний, сады',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Центральная проходная порта',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Вторая проходная',
+        price: 65,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Четвертая проходная',
+        price: 85,
+        location: 'Черноморск'
+    },
+    {
+        address: 'АЛДИ',
+        price: 55,
+        location: 'Черноморск'
+    },
+    {
+        address: 'ИВТ Авто ЧИП Гранит',
+        price: 45,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Управление порта',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: 'СЭС',
+        price: 65,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Транспортная',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: 'МЖК',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Кемпинг',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Торнадо',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: '9 микрорайон',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Приморская до 42, 43а',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Приморская с 44, 45',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Фабрика кормов, бугово кладбище',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: '114-й Радужный',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Береговая и Ст.Бугово',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: 'РСУ-55',
+        price: 50,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Водохоз. Маслоэкстр. Автобаза',
+        price: 50,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Южный',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: 'ИЗА - Таможня',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Квант, ИВТ БОНД АТП, ФОК',
+        price: 50,
+        location: 'Черноморск'
+    },
+    {
+        address: 'СТО Россь',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Доминика',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Ал-кая 23',
+        price: basePrice,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Ал-кая церковь, СТО, Бастион',
+        price: baseFive,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Ал-кая конец',
+        price: 50,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Моря Камея',
+        price: 50,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Юбилейный',
+        price: 60,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Лукоморье',
+        price: 55,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Сокол',
+        price: 55,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Дружба на верху спуск+5',
+        price: 55,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Дружба спуск',
+        price: 60,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Авиоремонтник',
+        price: 60,
+        location: 'Черноморск'
+    },
+    {
+        address: 'Авиоремонтник спуск',
+        price: 65,
+        location: 'Черноморск'
+    },
+
+    //Новое бугово
+
+    {
+        address: 'Одесская Парус',
+        price: basePrice,
+        location: 'Новое бугово'
+    },
+    {
+        address: 'Луговая',
+        price: '45-60',
+        location: 'Новое бугово'
+    },
+    {
+        address: 'Елочная 1',
+        price: 50,
+        location: 'Новое бугово'
+    },
+    {
+        address: 'Школьный переулок 5',
+        price: 50,
+        location: 'Новое бугово'
+    },
+    {
+        address: 'Стройматериалы В/Ч',
+        price: 60,
+        location: 'Новое бугово'
+    },
+    {
+        address: 'Птицефабрика',
+        price: 70,
+        location: 'Новое бугово'
+    },
+    {
+        address: 'Катюшка Лунная',
+        price: 65,
+        location: 'Новое бугово'
+    },
+
+    //Молодежное
+
+    {
+        address: 'Бедный поселок',
+        price: 65,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Молодежное Точка',
+        price: 65,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Молодежное Точка Заезд',
+        price: 85,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Тракторная Бригада',
+        price: 65,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Санжейская дорога 3',
+        price: 65,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Кладбище',
+        price: 90,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Переезд Волна',
+        price: 80,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Цветной бульвар',
+        price: baseFive,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Солнечная за пожаркой',
+        price: baseFive,
+        location: 'Молодежное'
+    },
+    {
+        address: 'Новые нарезки',
+        price: 75,
+        location: 'Молодежное'
+    },
+
+    //Александровка
+    {
+        address: 'Остановка Александровка + заезд 25грн',
+        price: 90,
+        location: 'Александровка'
+    },
+    {
+        address: '15 ВПТУ',
+        price: 85,
+        location: 'Александровка'
+    },
+    {
+        address: 'Шипка',
+        price: 95,
+        location: 'Александровка'
+    },
+    {
+        address: 'Огородная',
+        price: 105,
+        location: 'Александровка'
+    },
+    {
+        address: 'ИСРЗ',
+        price: 85,
+        location: 'Александровка'
+    },
+    {
+        address: 'Поликлиника',
+        price: 85,
+        location: 'Александровка'
+    },
+    {
+        address: 'Точка по Ал-ке',
+        price: basePrice,
+        location: 'Александровка'
+    },
+
+    //Малодолинское
+    {
+        address: 'Остановка Малая',
+        price: 110,
+        location: 'Малодолинское'
+    },
+    {
+        address: 'Паромная переправа',
+        price: '120-145',
+        location: 'Малодолинское'
+    },
+    {
+        address: 'Кладбище Малая',
+        price: 160,
+        location: 'Малодолинское'
+    },
+    {
+        address: 'Восход ',
+        price: 135,
+        location: 'Малодолинское'
+    },
+    {
+        address: 'Ява',
+        price: 165,
+        location: 'Малодолинское'
+    },
+    {
+        address: 'Бурлачья Балка',
+        price: 130,
+        location: 'Малодолинское'
+    },
+    {
+        address: 'Рыб порт',
+        price: 170,
+        location: 'Малодолинское'
+    },
+    {
+        address: 'с.о Юбилейные',
+        price: 145,
+        location: 'Малодолинское'
+    },
+    //Великодолинское
+
+    {
+        address: 'Остановка Большая',
+        price: 160,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Аккаржа ЖБК',
+        price: 120,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Маринский ЖК',
+        price: 120,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Военный Городок',
+        price: 160,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Заря',
+        price: 130,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Волна 1-2 линия',
+        price: 85,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Волна 3-10 линия',
+        price: 100,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Волна 11-20 линия',
+        price: 110,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Волна 21-30 линия',
+        price: 140,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Волна 31-33 линия',
+        price: 150,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Ветеран 1/2/3',
+        price: '70/90/110',
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Ин-т Таирова',
+        price: 170,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Очистные',
+        price: 120,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Санжейка',
+        price: 150,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Леонидово',
+        price: 170,
+        location: 'Великодолинское'
+    },
+    {
+        address: 'Винно-коньячный завод',
+        price: 190,
+        location: 'Великодолинское'
+    },
+
+
+]
